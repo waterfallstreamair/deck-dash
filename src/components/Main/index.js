c7fe7ea71dba084abce72d982c3f95c7e8c98c49
Alex Waterfall <waterfallstreamair@gmail.com>
2018-12-06 15:14:57 +0000

    Project init

---
+++ src/components/Main/index.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+const Main = styled.div`
+`;
+
+export default Main;
---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BoardPage from './BoardPage';
+import H1 from '../components/H1';
+import Main from '../components/Main';
+
+const App = () => (
+  <Main>
+    <H1>HR Users 2</H1>
+    <BoardPage />
+  </Main>
+);
+
+export default App;
