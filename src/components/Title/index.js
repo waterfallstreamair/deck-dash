8d71b6ceb73270eabf2f9f1381c0540522869080
Alex Waterfall <waterfallstreamair@gmail.com>
2018-12-06 16:14:51 +0000

    add title component

---
+++ src/components/Title/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Title = styled.div`
+  color: #cccccc;
+  text-align: center;
+  padding-top: 0.25em;
+  display: inline-block;
+  width: 90%;
+`;
+
+export default Title;
