e66a705cbd0a7e33854dcd46964973712107ef48
Alex Waterfall <waterfallstreamair@gmail.com>
2018-12-07 19:51:20 +0000

    clean up

--- src/components/Remove/index.js
+++ src/components/Remove/index.js
@@ -2,9 +2,6 @@ import styled from 'styled-components';
 
 const Remove = styled.a`
   font-size: 1em;
-  color: #cccccc;
-  width: 5%;
-  right: 0.1em;
   width: 1.5em;
   height: 1.5em;
   text-align: right;
